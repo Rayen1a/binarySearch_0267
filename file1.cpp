5c32294557447fb05ec53a1f651679bd653a27e4
Rayen1a <raihan01426@gmail.com>
2025-05-02 08:05:37 +0700

    memasukkan void binarySearch

--- file1.cpp
+++ file1.cpp
@@ -5,7 +5,13 @@ int element[10];
 int nPanjang;
 int x;
 
-
+void binarySearch() {
+    cout << "Masukkan element yang ingin dicari = " << endl;
+    cin >> x; // step 1
+    int low = 0; // step 2
+    int high = nPanjang-1; // step 3
+    int mid = (low + high)/2; // step 4
+}
 
 int main(){
 
